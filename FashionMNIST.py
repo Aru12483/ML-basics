9dc7b542dd6a37db0ea7ad2197ac21a519d61439
Aru12483 <100664068+Aru12483@users.noreply.github.com>
2022-07-11 11:15:57 +0530

    Update FashionMNIST.py

--- FashionMNIST.py
+++ FashionMNIST.py
@@ -1,120 +1,134 @@
-# TensorFlow and tf.keras
-from sklearn import metrics
+import sys
+from matplotlib import pyplot
+from keras.datasets import fashion_mnist
+from tensorflow.keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from tensorflow.keras.optimizers import SGD
 import tensorflow as tf
+from keras.layers import BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import regularizers 
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
 
-print(tf.__version__)
+# load train and test dataset
+def load_dataset():
+	# load dataset
+	(trainX, trainY), (testX, testY) = fashion_mnist.load_data()
+	# one hot encode target values
+	trainY = to_categorical(trainY)
+	testY = to_categorical(testY)
+	return trainX, trainY, testX, testY
 
-fashion_mnist = tf.keras.datasets.fashion_mnist
+# scale pixels
+def prep_pixels(train, test):
+	# convert from integers to floats
+	train_norm = train.astype('float32')
+	test_norm = test.astype('float32')
+	# normalize to range 0-1
+	train_norm = train_norm / 255.0
+	test_norm = test_norm / 255.0
+	# return normalized images
+	return train_norm, test_norm
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# define cnn model
+def define_model():
+	model = Sequential()
+	model.add(Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(28, 28, 1)))
+	model.add(Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.22))
+	model.add(Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.22))
+	model.add(Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(BatchNormalization())
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.22))
+	#model.add(Conv2D(256, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	#model.add(Conv2D(256, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	#model.add(MaxPooling2D((2, 2)))
+	#model.add(Dropout(0.2))
+	#model.add(Conv2D(512, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	#model.add(Conv2D(512, (2, 2), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	#model.add(MaxPooling2D((2, 2)))
+	#model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'),kernel_regularizer=regularizers.L1L2(l1=0.1, l2=0.1),bias_regularizer=regularizers.L2(0.01),activity_regularizer=regularizers.L2(0.01))
+	model.add(Dropout(0.22))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(learning_rate=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+	# plot loss
+	pyplot.subplot(211)
+	pyplot.title('Cross Entropy Loss')
+	pyplot.plot(history.history['loss'], color='blue', label='train')
+	pyplot.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	pyplot.subplot(212)
+	pyplot.title('Classification Accuracy')
+	pyplot.plot(history.history['accuracy'], color='blue', label='train')
+	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save plot to file
+	filename = sys.argv[0].split('/')[-1]
+	pyplot.savefig(filename + '_plot.png')
+	pyplot.close()
 
-train_images.shape
-len(train_labels)
-train_labels
-test_images.shape
-len(test_labels)
+# run the test harness for evaluating a model
+def run_test_harness():
+	# load dataset
+	trainX, trainY, testX, testY = load_dataset()
+	# prepare pixel data
+	trainX, testX = prep_pixels(trainX, testX)
+	# define model
+	model = define_model()
+	# fit model
+	history = model.fit(trainX, trainY, epochs=100, batch_size=64, validation_data=(testX, testY), verbose=0)
+	# evaluate model
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history)
 
-# plot the MNIST image having pixel values between 0-255
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+# entry point, run the test harness
+run_test_harness()
 
-#PreProcessing Stage 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+#IDEAL SCENARIO
+# genererally Relu as an activation function and he uniform are considered as the norm as they provide better results -> why? 
+# experimentally attained value that ReLu + he_uniform is the best performing initializer and activation function combo 
 
-# show first 10 training images 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(True)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
 
-# Building the model 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28,)),
-    tf.keras.layers.Dense(128,activation='relu'),
-    tf.keras.layers.Dense(10)
-])
+# CHANGE IN INITIALIZERS
+# inital accuracy with 4 stacks of comvolution layers  with he uniform is : 92.990 %
+# changing kernel_initalizer from he uniform to normally initialised : resulted in accuracy of 92.030 so slight drop and it took 13mins 24 seconds instead of 12 mins approx 
+# changing the kernel_initializer to GlorotNormal : accuracy -> 92.020% and same time of 13 mins and 24 seconds 
+# changing initialiser to lecun_normal dropped accuracy even further to 92.1290 % time of execution remains unchanged 
+# changing the initialiser to variance scaling : 92.520
+# changing to lecun_uniform 92.080 % time taken is the same 
+# changing to he_normal gives 92.660 accuracy 
 
-#compilation of the model:
-model.compile(optimizer = 'Adam',
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = 'True'),
-metrics=['Accuracy'])
+#CHANGE IN BLOCKS FROM 4 TO 5 BLOCKS : 32-64-128-256-512 LAYERS WITH DROPOUTS MAX POOLING LAST LAYER IS FLATTENED AND CONNECTED TO A FULLY CONNECTED LAYER 
+# Accuracy drops after adding another layer and computation time goes up to 21 minutes and 34 seconds 91.798
+# dropping a layer and adding a 256 dense layer fully connected : drops accuracy to 92.720 still lower than initial runtime the same as 4 layers previous
 
-#training the model :
-
-# feeding model / fitting training data to model 
-model.fit(train_images,train_labels,epochs = 10)
-
-# evaluating accuracy :
-test_loss, test_acc = model.evaluate(test_images,test_labels,verbose = 2)
-print(test_acc)
-
-probability_model = tf.keras.Sequential([model,tf.keras.layers.Softmax()])
-predictions = probability_model.predict(test_images)
-print(predictions[0])
-print(np.argmax(predictions[0]))
-# Model is sure that at index 0 there is label 9 (ankle boot)
-print(test_labels[0])
-
-#plotting the images
-
-def plot_image(i, predictions_array, true_label, img):
-  true_label, img = true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-
-  plt.imshow(img, cmap=plt.cm.binary)
-
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
-
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-  true_label = true_label[i]
-  plt.grid(False)
-  plt.xticks(range(10))
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
-
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
-
-  i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-i = 8
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
+#CHANGE IN REGULARISER 
+# adding activity kernel and bias regularizer with l1 and l2 values as 0.1 
